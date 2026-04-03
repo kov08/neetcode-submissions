68a6d520bd0149f39c295c36a923f2ca890f4281
Krushnarajsinh Vansia <69028031+kov08@users.noreply.github.com>
2026-04-03 11:58:35 -0400

    Add: is-anagram - submission-3

---
+++ Data Structures & Algorithms/is-anagram/submission-3.java
@@ -0,0 +1,28 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        // return false if the size of the given strings are different
+        if(s.length() != t.length()){
+            return false;
+        }
+
+        // Declare an array of ch 26 for all 26 char
+        int[] arr = new int[26];
+
+        // run a loop to 
+        for(int i = 0; i < s.length(); i++){
+            arr[(int)(s.charAt(i)-97)] += 1;
+            arr[(int)(t.charAt(i)-97)] -= 1;
+        }
+
+        for(int i: arr){
+            if(i != 0){
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
+
+// TC: O(N) N: length of the String
+// SC: O(M) where M: 27 for the arr array of alphabates
