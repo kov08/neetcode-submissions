3bc446c37bdc0e0cf9ab281cfef865c98189db2e
Krushnarajsinh Vansia <69028031+kov08@users.noreply.github.com>
2026-05-07 12:01:42 -0400

    Add: climbing-stairs - submission-5

---
+++ Data Structures & Algorithms/climbing-stairs/submission-5.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int climbStairs(int n) {
+        if( n <= 2) return n;
+        // divide problem into number of smaller problem that either take 1 or 2 steps 
+        // repeatatively perform this step until reached the given number
+        // this leads to recurssion
+
+        // Space ooptimize by saving only two numbers as in all iterartion we need only 2 numbers
+        int oneStep = 1;
+        int twoStep = 2;
+
+        // Bottom up approach where we start from  bottom 2 and reached till the given number
+        for(int i = 3; i <= n; i++){
+            int temp = oneStep + twoStep;
+            oneStep = twoStep;
+            twoStep = temp;
+        }
+        return twoStep;
+    }
+}
+
+// TC O(n)
+// SC O(1)
