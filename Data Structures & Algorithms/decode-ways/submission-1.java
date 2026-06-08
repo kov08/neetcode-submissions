02c4b723f5be24056c3b9700aa0582ad5712f261
Krushnarajsinh Vansia <69028031+kov08@users.noreply.github.com>
2026-06-08 17:56:32 -0400

    Add: decode-ways - submission-1

---
+++ Data Structures & Algorithms/decode-ways/submission-1.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int numDecodings(String s) {
+        Integer[] dp = new Integer[s.length()];
+        return s.length() == 0 ? 0 : numDecodings(0, s, dp);      
+    }
+    private int numDecodings(int p, String s, Integer[] dp) {
+        int n = s.length();
+        if(p == n) return 1;
+        if(s.charAt(p) =='0') return 0;
+
+        if(dp[p] != null) return dp[p];
+        int res= numDecodings(p+1, s, dp);
+        if(p < n-1 && (s.charAt(p) == '1'|| s.charAt(p) == '2' && s.charAt(p+1) < '7')) 
+			res += numDecodings(p+2, s, dp);
+        dp[p] = res;;
+        return dp[p]; 
+    }
+}
