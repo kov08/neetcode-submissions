7b71b5f0532a4aab63e3460c5695287d9e0da841
Krushnarajsinh Vansia <69028031+kov08@users.noreply.github.com>
2026-06-11 20:25:26 -0400

    Add: course-schedule - submission-12

---
+++ Data Structures & Algorithms/course-schedule/submission-12.java
@@ -0,0 +1,40 @@
+class Solution {
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        Map<Integer, List<Integer>> pre = new HashMap<>();
+        for(int[] p : prerequisites){
+            int course = p[0];
+            int req = p[1];
+            pre.computeIfAbsent(course, r-> new ArrayList<>()).add(req);
+        } 
+        int[] vis = new int[numCourses];
+        // 3 states
+        // 0 unvisited
+        // 1 visiting
+        // 2 visited
+        for(int i = 0; i< numCourses; i++){
+            if(!dfs(i, pre, vis)){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private boolean dfs(int course, Map<Integer, List<Integer>> pre, int[] vis){
+        
+        if(vis[course] == 2) return true;
+        if(vis[course] == 1) return false;
+        
+        vis[course] = 1;
+        List<Integer> ls = pre.get(course);
+        if(ls != null) {
+            for(int i : ls){
+                if(!dfs(i, pre, vis)){
+                    return false;
+                }
+            }
+        }
+        vis[course] = 2;
+        return true;
+    }
+}
+
