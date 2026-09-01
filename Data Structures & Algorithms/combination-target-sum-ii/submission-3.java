46582a5ac5f4a8ed3decdff048798259e249b273
Krushnarajsinh Vansia <69028031+kov08@users.noreply.github.com>
2026-09-01 07:27:41 -0400

    Add: combination-target-sum-ii - submission-3

---
+++ Data Structures & Algorithms/combination-target-sum-ii/submission-3.java
@@ -0,0 +1,36 @@
+class Solution {
+    public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+        Arrays.sort(candidates);
+        List<List<Integer>> ans =  new ArrayList<>();
+        List<Integer> lst = new ArrayList<>();
+        rec(0, target, candidates, lst, ans);
+        return ans;
+    }
+
+    private void rec(int i, int t, int[] nums, List<Integer> lst, List<List<Integer>> ans){
+        if(t == 0){
+            ans.add(new ArrayList<>(lst));
+            return;
+        } else if(i == nums.length || t < 0 || t < nums[i]){
+            return;
+        } 
+
+        for(int j = i; j < nums.length; j++){
+            if(j > i && nums[j] == nums[j - 1]) continue;
+            // take
+            lst.add(nums[j]);
+            rec(j+1, t - nums[j], nums, lst, ans);
+            lst.remove(lst.size() - 1);
+        }
+
+        // Not take NO DUplicates
+        // while(i+1 < nums.length && nums[i+1] == nums[i]){
+        //     i++;
+        // }
+        // rec(i+1, t, nums, lst, ans);
+        
+    }
+}
+
+// TC O(N*2^N)
+// SC O(N)
